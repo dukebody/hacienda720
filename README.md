d47c5a0fe98f548aa2e0a493e6ff72b2a0522bef
Israel Saeta Pérez <dukebody@gmail.com>
2024-01-25 19:55:20 +0100

    Añadir opción de considerar declaración del año pasado.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Utilidades para generar un archivo 720 para importar en la herramienta de Hacien
 Ahora mismo sólo hay un módulo para leer la hoja de balance de Mintos. Idealmente se puede extender fácilmente a otras plataformas, ya sea crowd o bolsa.
 
 Instrucciones de uso:
-- Cambiar las variables en mayúsculas de arriba del archivo `mintos.py` con los datos propios. Se espera que el archivo INPUT_FILENAME esté en el mismo directorio del script.
+- Cambiar las variables en mayúsculas de arriba del archivo `mintos.py` con los datos propios. Se espera que los archivos de entrada estén en el mismo directorio del script.
 - Ejecutar `python mintos.py`. Generará un archivo con el nombre `OUTPUT_FILENAME`, que se puede importar en la herramienta web de Hacienda.
--- mintos.py
+++ mintos.py
@@ -1,16 +1,24 @@
 from csv import DictReader
 from decimal import Decimal
 
+OPERATION_ACQUISITION = "A"
+OPERATION_MODIFICATION = "M"
+OPERATION_CANCELLATION = "C"
 
-# Cambiar estos datos
-INPUT_FILENAME = "Mintos Investor Loan Balances on Specific Date.xlsx.csv"
+
+## CAMBIAR ESTOS DATOS DE ABAJO
+MINTOS_LOAN_BALANCES_FILE = "Mintos Investor Loan Balances 2023.csv"
+
+# si no se ha declarado el año pasado, dejar esto como ""
+MINTOS_LOAN_BALANCES_PREVIOUS_EXERCISE_FILE = "Mintos Investor Loan Balances 2023.csv"
+
 OUTPUT_FILENAME = "test.720"
 
-YEAR = "2022"
+YEAR = "2023"
 NAME = "NOMBRE APELLIDO1 APELLIDO2"
 DNI = "21928208P"
 PHONE = "600112233"
-
+## CAMBIAR ESTOS DATOS DE ARRIBA
 
 
 def comma_to_dot(amount):
@@ -41,26 +49,58 @@ def get_asset_data_from_mintos_balance_sheet(filename):
 
     return assets
 
-LINE_1 = "1720{YEAR}{DNI}{NAME:40}T{PHONE}{NAME:40}7200000000000  {n_entries:0>22} {total_amount:0>17.0f} 00000000000000000                                                                                                                                                                                                                                                                                                                                \n"
-LINE_N = "2720{YEAR}{DNI}{DNI}         {NAME:40}1                         V2                         LV1{isin}                                              {issuer_name:24}                 {issuer_registration_number}                                                                                                                                                                           LV00000000A00000000 {amount:0>14.0f} 00000000000000A{n_notes:0>12.0f} 10000                    \n"
-def write_720_file_from_assets(assets, filename):
+
+LINE_1 = "1720{YEAR}{DNI}{NAME:40}T{PHONE}{NAME:40}7200000000000  {n_entries:0>22} {total_amount_cents:0>17.0f} 00000000000000000                                                                                                                                                                                                                                                                                                                                \n"
+LINE_N = "2720{YEAR}{DNI}{DNI}         {NAME:40}1                         V2                         LV1{isin}                                              {issuer_name:24}                 {issuer_registration_number}                                                                                                                                                                           LV00000000{operation}00000000 {amount_cents:0>14.0f} 00000000000000A{n_values_cents:0>12.0f} 10000                    \n"
+def write_720_file_from_assets(filename, assets, assets_previous_exercise={}):
     """
     Escribe archivo tipo 720 con nombre filename a partir de una lista de assets
     """
-    # generar cada línea del 720
     lines = [LINE_1.format(
         YEAR=YEAR, DNI=DNI, NAME=NAME, PHONE=PHONE,
         n_entries=len(assets),
-        total_amount=sum(asset["amount"]*100 for asset in assets.values())
+        total_amount_cents=sum(asset["amount"]*100 for asset in assets.values())
     )]
-    for isin, value in assets.items():
+    isins = set(assets.keys())
+    isins_previous_exercise = set(assets_previous_exercise.keys())
+
+    all_isins = isins | isins_previous_exercise
+    new_isins = isins - isins_previous_exercise
+    modified_isins = isins & isins_previous_exercise
+    cancelled_isins = isins_previous_exercise - new_isins
+
+    # generar cada línea del 720
+    for isin in all_isins:
+        if isin in new_isins:
+            operation = OPERATION_ACQUISITION
+            value = assets[isin]
+            amount_cents = round(value["amount"]*100)
+        elif isin in modified_isins:
+            operation = OPERATION_MODIFICATION
+            value = assets[isin]
+            amount_cents = round(value["amount"]*100)
+        elif isin in cancelled_isins:
+            operation = OPERATION_CANCELLATION
+            value = assets_previous_exercise[isin]
+            amount_cents = 1
+        else:
+            raise Exception(f"Value {isin} not found!")
+
+        if amount_cents == 0:  # en caso de menos de 1 céntimo lo redondeamos hacia arriba
+            amount_cents = 1
+
+        # el n de valores se tiene que poner con 2 decimales y cada note es 0.01€
+        n_values_cents = amount_cents * 100
+
         line = LINE_N.format(
             YEAR=YEAR, DNI=DNI, NAME=NAME, 
             isin=isin, 
             issuer_name=value["issuer_name"],
             issuer_registration_number=value["issuer_registration_number"],
-            amount=value["amount"]*100,
-            n_notes=value["amount"]*10000,)
+            amount_cents=amount_cents,  # el monto se tiene que poner en céntimos de €
+            n_values_cents=n_values_cents,
+            operation=operation,
+        )
         lines.append(line)
 
     # guardar en archivo
@@ -68,5 +108,15 @@ def write_720_file_from_assets(assets, filename):
         file_handler.writelines(lines)
 
 
-assets = get_asset_data_from_mintos_balance_sheet(INPUT_FILENAME)
-write_720_file_from_assets(assets, OUTPUT_FILENAME)
+
+# cargar los activos del ejercicio a declarar
+assets = get_asset_data_from_mintos_balance_sheet(MINTOS_LOAN_BALANCES_FILE)
+
+# si hay una declaración anterior, importarla para marcar los activos como cancelados
+if MINTOS_LOAN_BALANCES_PREVIOUS_EXERCISE_FILE:
+    assets_previous_exercise = get_asset_data_from_mintos_balance_sheet(MINTOS_LOAN_BALANCES_PREVIOUS_EXERCISE_FILE)
+else:
+    assets_previous_exercise = {}
+
+
+write_720_file_from_assets(OUTPUT_FILENAME, assets, assets_previous_exercise)
